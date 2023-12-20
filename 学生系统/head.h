b18f4422e013d64b3f06a0cd215c0bac0fec66f3
红烧肉不太甜 <153347688+hsrbtt@users.noreply.github.com>
2023-12-20 20:21:44 +0800

    Add files via upload

--- 学生系统/head.h
+++ 学生系统/head.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <string.h> 
 typedef struct student{
 	int stunum;
 	char name[20];
@@ -11,6 +12,10 @@ typedef struct node{
 	student student;
 	struct node*next;
 }node;//定义节点信息 
+typedef struct account{
+	char id[20];
+	char an[20];
+} account; 
 void output();
 void inputstudent(node*head);
 void putoutstudent(node*head);
@@ -21,3 +26,4 @@ void delstudent(node*head);
 void sortstudent(node*head);
 void savestudent(node*head);
 node*readstudent(node*p);
+int login();
